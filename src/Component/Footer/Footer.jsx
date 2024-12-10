fb61aae712dceb0a062a1a57654e6a25c6977c19
MaestroDev007 <udohemma007@gmail.com>
2024-12-10 12:36:52 +0100

    new commit

--- src/Component/Footer/Footer.jsx
+++ src/Component/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="bg-[#001442] text-white px-10 pt-20">
+    <footer className="bg-[#001442] text-white px-10 pt-20 ">
       <figure>
         <img />
       </figure>
