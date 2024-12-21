4d9890127621fada35cb3decdbbc11dfe3b0297b
MaestroDev007 <udohemma007@gmail.com>
2024-12-21 15:26:00 +0100

    anal

--- src/Config/WashFoldModal.jsx
+++ src/Config/WashFoldModal.jsx
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import logo from ".././Images/SPARKULS-removebg-preview.png";
 import { useForm } from "react-hook-form";
+import emailjs from "@emailjs/browser";
 
 const WashFoldModal = ({ setShowWashFoldModal, showWashFold }) => {
   const phoneRegex = /^(\+)?(234|0)[0-9]*?.*/;
@@ -14,13 +15,30 @@ const WashFoldModal = ({ setShowWashFoldModal, showWashFold }) => {
     WashFoldModalRef.current === e.target ? setShowWashFoldModal(false) : null;
   };
 
-  const { register, handleSubmit } = useForm({
-    defaultValues: {
-      userName: "UserName",
-    },
+  const { register, handleSubmit, reset } = useForm({
+    // defaultValues: {
+    //   userName: "UserName",
+    // },
   });
 
-  const onSubmit = (data) => {
+  // emailjs
+  // .send(
+  //   "",
+  //   "",
+  //   emailParams,
+  //   ""
+  // )
+  // .then(
+  //   (response)=> {
+  //     alert("Email Sent Successfully");
+  //     reset()
+  //   },
+  //   (error) => {
+  //     alert("Failed to send Message")
+  //   }
+  // )
+
+  const sendToWhatsapp = (data) => {
     const Message = `Hello, My name is ${data.userName} from ${data.address}, requesting for a ${data.service}. \nHere is a description of my Laundry:`;
 
     const phoneNumber = "+2348061164856";
@@ -37,109 +55,131 @@ const WashFoldModal = ({ setShowWashFoldModal, showWashFold }) => {
       className={`${
         showWashFold ? "hidden" : "flex"
       } fixed inset-0 flex-col items-center justify-center backdrop-blur-sm bg-white/10 text-center z-50 mx-auto`}>
-      <div className="bg-black text-black p-2 rounded-lg shadow-md w-[250px] md:w-[350px] md:h-[650px] relative">
+      <div className="bg-black text-black p-2 rounded-lg shadow-md w-[300px] md:w-[650px] md:h-[650px] relative">
         <figure>
           <img
             src={logo}
             alt="Logo"
-            className="w-[120px] mx-auto"
+            className="w-[120px] mx-auto md:w-[150px]"
           />
         </figure>
-        <p className="text-white text-xs mt-4">Kindly Fill Out The Fields</p>
+        <p className="text-white text-xs mt-4 md:mt-1 md:text-base">
+          Kindly Fill Out The Fields
+        </p>
         <fieldset className="text-white mt-4 md:mt-10">
           <form
-            onSubmit={handleSubmit(onSubmit)}
-            className="flex flex-col items-center gap-4 text-sm w-full md:text-base md:items-start md:gap-10 md:px-4">
+            onSubmit={handleSubmit((data) => {
+              sendToWhatsapp(data);
+              // sendToGmail(data);
+            })}
+            className="flex flex-col items-center gap-4 text-sm w-full md:text-xl md:items-start md:gap-10 md:px-12">
             {/* Service Selection */}
-            <div className="flex flex-col gap-4 md:flex-row items-center">
-              <label
+            <div className="flex flex-col gap-4 md:flex-row items-center w-full">
+              {/* <label
                 htmlFor="Service"
-                className="block"
+                className=""
                 {...register("service", {
                   required: "Please select a service",
                 })}>
                 Service:
-              </label>
+              </label> */}
               <select
                 name="service"
                 required
-                className="text-black p-2 rounded text-xs">
+                className="text-black p-2 rounded text-xs w-full md:p-3 md:text-base md:mx-4">
                 <option>Dry Clean (3-4 days)</option>
                 <option>Express Pay As You Go (24hrs)</option>
               </select>
             </div>
 
-            {/* Full Name */}
-            <div className="flex flex-col gap-4 md:flex-row items-center">
-              <label
+            <div  className="flex flex-col gap-4 w-full md:flex-row md:mx-auto md:items-center md:justify-evenly">
+              {/* Full Name */}
+              <div className="flex flex-col gap-4 md:flex-row items-center">
+                {/* <label
                 htmlFor="Name"
                 className="block">
                 Full Name:
-              </label>
+              </label> */}
+                <input
+                  type="text"
+                  id="userName"
+                  placeholder="First Name"
+                  {...register("userName", {
+                    required: {
+                      value: "true",
+                      message: "Please Enter Full Name",
+                    },
+                  })}
+                  className="placeholder:text-xs backdrop-blur-md bg-white/20 text-white rounded-md outline-none p-2 placeholder:text-white/80 w-full md:p-3 md:placeholder:text-base"
+                />
+              </div>
+
+              <div>
               <input
-                type="text"
-                id="userName"
-                {...register("userName", {
-                  required: {
-                    value: "true",
-                    message: "Please Enter Full Name",
-                  },
-                })}
-                className="placeholder:text-xs text-black rounded-md outline-none p-1 border-2 focus:border-teal-500 "
-              />
+                  type="text"
+                  id="userName"
+                  placeholder="Last Name"
+                  {...register("userName", {
+                    required: {
+                      value: "true",
+                      message: "Please Enter Full Name",
+                    },
+                  })}
+                  className="placeholder:text-xs backdrop-blur-md bg-white/20 text-white rounded-md outline-none p-2 placeholder:text-white/80 w-full md:p-3 md:placeholder:text-base"
+                />
+              </div>
             </div>
 
             {/* Address */}
-            <div className="flex flex-col gap-4 md:flex-row items-center">
-              <label
+            <div className="flex flex-col gap-4 md:flex-row items-center w-full">
+              {/* <label
                 htmlFor="Address"
                 className="block">
                 Address:
-              </label>
+              </label> */}
               <input
                 type="text"
                 id="address"
+                placeholder="Address"
                 {...register("address", {
-                  required: {
-                    value: "true",
-                    message: "Please Fill In An Address",
-                  },
+                  required:  "Please Fill In An Address",
+                  
                 })}
-                className="placeholder:text-xs text-black rounded-md outline-none p-1 border-2 focus:border-teal-500 "
+                className="placeholder:text-xs backdrop-blur-md bg-white/20 text-white rounded-md outline-none p-2 placeholder:text-white/80 w-full md:p-3 md:placeholder:text-base md:mx-4"
               />
             </div>
 
             {/* Phone Number */}
-            <div className="flex flex-col gap-4 md:flex-row items-center">
-              <label
+            <div className="flex flex-col gap-4 md:flex-row items-center w-full">
+              {/* <label
                 htmlFor="Phone"
                 className="block">
                 Phone Number:
-              </label>
+              </label> */}
               <input
                 type="text"
+                placeholder="Phone Number"
                 id="phone"
                 {...register("phone", {
                   required: "Please enter your phone number",
                   validate: (value) =>
                     phoneRegex.test(value) || "Invalid phone number",
                 })}
-                placeholder="080123456789"
-                className="placeholder:text-xs text-black rounded-md outline-none p-1 border-2 focus:border-teal-500 "
+                className="placeholder:text-xs backdrop-blur-md bg-white/20 text-white rounded-md outline-none p-2 placeholder:text-white/80 w-full md:p-3 md:placeholder:text-base md:mx-4"
               />
             </div>
 
             {/* Carriage Size */}
-            <div className="flex flex-col gap-4 md:flex-row items-center">
-              <label
+            <div className="flex flex-col gap-4 md:flex-row items-center w-full">
+              {/* <label
                 htmlFor="Carriage"
                 className="block">
                 Carriage Size:
-              </label>
+              </label> */}
               <select
                 name="carriageSize"
                 {...register("service", { required: "This field is required" })}
-                className="text-black p-2 rounded text-xs">
+                className=" p-2 rounded text-xs bg-white  text-black w-full md:p-3 md:mx-4 md:text-base">
                 <option>Bike Size Carriage</option>
                 <option>Vehicle Size Carriage</option>
               </select>
@@ -158,12 +198,20 @@ const WashFoldModal = ({ setShowWashFoldModal, showWashFold }) => {
                 Terms Of Service
               </label>
             </div>
-            <button
-              type="submit"
-              onSubmit={handleSubmit(onSubmit)}
-              className="text-black bg-white py-2 px-8 font-semibold rounded-xl mx-auto flex items-center hover:">
-              Submit
-            </button>
+            <div className="flex gap-6">
+              <button
+                type="submit"
+                onSubmit={handleSubmit(sendToWhatsapp)}
+                className="text-black bg-white py-2 px-4 font-semibold rounded-xl mx-auto flex items-center hover:">
+                Send To Whatsapp
+              </button>
+              <button
+                type="submit"
+                onSubmit={handleSubmit(sendToWhatsapp)}
+                className="text-black bg-white py-2 px-4 font-semibold rounded-xl mx-auto flex items-center hover:">
+                Send To Gmail
+              </button>
+            </div>
           </form>
         </fieldset>
       </div>
